3c8a6792778d5f17005d0b761930b8967109d72a
BornToWard <julien.his@viacesi.fr>
2016-12-08 12:16:34 +0100

    Update fonctions.c

--- fonctions.c
+++ fonctions.c
@@ -2,3 +2,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "fonctions.h"
+
+int my_rand(int x);
+{
+    srand(time(NULL));
+    int randomValue;
+    randomValue=rand()%x;
+
+    return(randomValue);
+}
