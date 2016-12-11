f3ac5dc7ead6d6c57a5d40980ab09224df10b790
BjornDaft <romain.jouatte@viacesi.fr>
2016-12-11 12:05:55 +0100

    avion alpha 0.2

--- avion interractif.c
+++ avion interractif.c
@@ -1,55 +1,62 @@
 #include <stdio.h>
 #include <sys/ioctl.h>
 #include <stdlib.h>
-
+#include <time.h>
+ 
 #define KEYBOARDU 8
 #define KEYBOARDD 2
 #define KEYBOARDR 6
 #define KEYBOARDL 4
-
+#define KEYBOARDEXIT 120
+ 
+ 
 struct winsize w;
  
 int taille()
 {
-  ioctl(0, TIOCGWINSZ, &w); /*give widht with w.ws_row and length with w.ws_col */
+  ioctl(0, TIOCGWINSZ, &w); /*give width with w.ws_row and length with w.ws_col */
   return(0);
 }
-
-int planedisplay(char fill, int position, int w)
+ 
+int rand_(int a)
+{
+  time_t t;
+  srand((unsigned)time(&t));
+  return rand()%(a)+1;
+}
+ 
+int arrondi(float a)
+{
+  return a +0.5;
+}
+int planedisplay(int keys, int planewidth, int planelength)
 {
   printf("\033c"); /*clean the terminal*/
-  char cursor = a;
-  char picture
-  switch(fill) /*open the right picture choose by the user*/
+  FILE* fichier = NULL;
+  switch(keys) /*open the right picture choose by the user*/
   {
-    case 'u':
-    FILE* uplane = NULL;
-    uplane = fopen("upplane.pbm", "r");
-    picture = uplane
-    case 'd':
-    FILE* dplane = NULL;
-    dplane = fopen("downplane.pbm", "r");
-    picture = dplane
-    case 'r':
-    FILE* rplane = NULL;
-    rplane = fopen("rightplane.pbm", "r");
-    picture = rplane
-    case 'l':
-    FILE* uplane = NULL;
-    uplane = fopen("leftplane.pbm", "r");
-
+    case KEYBOARDU:
+    fichier = fopen("upplane.pbm", "r");
+    case KEYBOARDD:
+    fichier = fopen("downplane.pbm", "r");
+    case KEYBOARDR:
+    fichier = fopen("rightplane.pbm", "r");
+    case KEYBOARDL:
+    fichier = fopen("leftplane.pbm", "r");
+ 
   }
-
-   if (picture != NULL) /*display the plane on the screen*/
+ 
+   if (fichier != NULL) /*display the plane on the screen*/
     {
       int i = 0;
       int j = 0;
-      fseek(picture, 7, SEEK_SET);
+      int cursor;
+      fseek(fichier, 7, SEEK_SET);
       for(j =0;j<6;j++)
         {
           for(i =0;i<12;i++)
         {
-          cursor = fgetc(picture); /*take off the code character of the bpm file*/
+          cursor = fgetc(fichier); /*take off the code character of the bpm file*/
           switch(cursor) /*write a special character in the console*/
             {
             case '0':
@@ -64,7 +71,7 @@ int planedisplay(char fill, int position, int w)
          }
          }
  
-        fclose(picture); /*close the bpm file*/
+        fclose(fichier); /*close the bpm file*/
     }
     else
       {
@@ -73,26 +80,72 @@ int planedisplay(char fill, int position, int w)
     return 0;
 }
  
+ 
+ 
 int main(void)  
 {
+  int planewidth;
+  int planelength;
+  int initdirection;
+  int hotkey;
+  int newhotkey;
+  int oldplanewidth;
+  int oldplanelength;
+  int oldconsolewidth;
+  int oldconsolelength;
     taille();
-    p.widht = rand_a_b(0, w.ws_row) /*choose a random position in the console*/
-    p.length = rand_a_b(0, w.ws_col)
-    char hotkey 
-    char newhotkey
-    
-    while(newhotkey != x)
+    oldconsolewidth = w.ws_row;
+    oldconsolelength =w.ws_col;
+    planewidth = rand_(w.ws_row); /*choose a random position in the console*/
+    oldplanewidth = planewidth;
+    planelength = rand_(w.ws_col);
+    oldplanelength =planelength;
+    initdirection = rand_(4);
+    switch(initdirection)
+      {
+      case 1:
+    newhotkey = KEYBOARDU;
+      case 2:
+    newhotkey = KEYBOARDR;
+      case 3:
+    newhotkey = KEYBOARDD;
+      case 4:
+    newhotkey = KEYBOARDL;
+      }
+ 
+    /*begin while*/
+    while(newhotkey != KEYBOARDEXIT)
     {
-    	planedisplay(newhotkey, p, w)
-    	newhotkey = getchar();
-    	if(newhotkey!=8 && newhotkey!=6 && newhotkey!=2 && newhotkey!=4 && newhotkey!=x) /*if the character choose by the user is wrong we */
-    	{
-    		newhotkey=hotkey
-    	}
-    	else 
-    	{
-    		hotkey=newhotkey
-    	}
+      taille();
+      planewidth=(w.ws_row/(oldconsolewidth/oldplanewidth));
+      planewidth=arrondi(planewidth);
+      planelength=(w.ws_col/(oldconsolelength/oldplanelength));
+      planelength=arrondi(planelength);
+      oldconsolewidth = w.ws_row;
+      oldconsolelength = w.ws_col;       
+      planedisplay(newhotkey, planewidth, planelength);
+ 
+     
+        newhotkey = getchar();
+        if(newhotkey!=KEYBOARDU && newhotkey!=KEYBOARDD && newhotkey!=KEYBOARDR && newhotkey!=KEYBOARDL && newhotkey!=KEYBOARDEXIT) /*if the character choose by the user is wrong we don't change the position*/
+        {
+      newhotkey=hotkey;
+        }
+        else
+        {
+      hotkey=newhotkey;
+       switch(hotkey)
+            {
+            case KEYBOARDU:
+              planewidth=planewidth-1;
+            case KEYBOARDD:
+              planewidth=planewidth+1;
+            case KEYBOARDR:
+              planelength=planelength+1;
+            case KEYBOARDL:
+              planelength=planelength-1;
+            }
+        }
     }
     return 0;
 }
