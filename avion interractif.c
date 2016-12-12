2d285621d203b85ee8a1da1c99d7e374863c28b3
BjornDaft <romain.jouatte@viacesi.fr>
2016-12-12 10:42:54 +0100

    avion alpha 0.4

--- avion interractif.c
+++ avion interractif.c
@@ -3,14 +3,14 @@
 #include <stdlib.h>
 #include <time.h>
  
-#define KEYBOARDU 8
+#define KEYBOARDU 8 /*variable system for the keys*/
 #define KEYBOARDD 2
 #define KEYBOARDR 6
 #define KEYBOARDL 4
 #define KEYBOARDEXIT 120
  
  
-struct winsize w;
+struct winsize w; /*give the height of the console*/
  
 int taille()
 {
@@ -18,22 +18,29 @@ int taille()
   return(0);
 }
  
-int rand_(int a)
+int rand_(int a)   /*give a random number between 0 and a*/
 {
   time_t t;
   srand((unsigned)time(&t));
   return rand()%(a)+1;
 }
  
-int arrondi(float a)
+int arrondi(float a)  /*round a number*/
 {
   return a +0.5;
 }
-int planedisplay(int keys, int planewidth, int planelength)
+
+int mysleep(float temps)
+{
+	clock_t arrivee=clock()+(temps*CLOCKS_PER_SEC); // On calcule le moment où l'attente devra s'arrêter
+    while(clock()<arrivee);
+}
+
+int planedisplay(int keys, int planewidth, int planelength) /*display the plane according of the position and direction*/
 {
   printf("\033c"); /*clean the terminal*/
-  FILE* fichier = NULL;
-  switch(keys) /*open the right picture choose by the user*/
+  FILE* fichier = NULL; /*initializes a poiteur*/
+  switch(keys) /*open the picture according of the direction*/
   {
     case KEYBOARDU:
     fichier = fopen("upplane.pbm", "r");
@@ -51,13 +58,13 @@ int planedisplay(int keys, int planewidth, int planelength)
       int i = 0;
       int j = 0;
       int cursor;
-      fseek(fichier, 7, SEEK_SET);
-      for(j =0;j<6;j++)
+      fseek(fichier, 7, SEEK_SET); /*moves the read curseur to don't read the first lines*/
+      for(j =0;j<6;j++)  
         {
           for(i =0;i<12;i++)
         {
           cursor = fgetc(fichier); /*take off the code character of the bpm file*/
-          switch(cursor) /*write a special character in the console*/
+          switch(cursor) /*write a special character in the console according of the bpm file*/
             {
             case '0':
               printf(" ");
@@ -73,16 +80,16 @@ int planedisplay(int keys, int planewidth, int planelength)
  
         fclose(fichier); /*close the bpm file*/
     }
-    else
+    else /*if we can't open the file*/
       {
-    printf("\nErreur d'ouverture\n");
+    printf("\nOpening Error\n");
       }
     return 0;
 }
  
  
  
-int main(void)  
+int main(int argc, char *argv[])  
 {
   int planewidth;
   int planelength;
@@ -93,15 +100,15 @@ int main(void)
   int oldplanelength;
   int oldconsolewidth;
   int oldconsolelength;
-    taille();
-    oldconsolewidth = w.ws_row;
+    taille(); /*give the height of the console*/
+    oldconsolewidth = w.ws_row; /*save the console */
     oldconsolelength =w.ws_col;
     planewidth = rand_(w.ws_row); /*choose a random position in the console*/
-    oldplanewidth = planewidth;
+    oldplanewidth = planewidth; /*save the old value*/
     planelength = rand_(w.ws_col);
     oldplanelength =planelength;
-    initdirection = rand_(4);
-    switch(initdirection)
+    initdirection = rand_(4); /*choose a number between 1,2,3,4*/
+    switch(initdirection) /*choose the plane direction according of initdirection*/
       {
       case 1:
     newhotkey = KEYBOARDU;
@@ -114,19 +121,19 @@ int main(void)
       }
  
     /*begin while*/
-    while(newhotkey != KEYBOARDEXIT)
+    while(newhotkey != KEYBOARDEXIT) /*run while the user don't tape the x keys*/
     {
-      taille();
-      planewidth=(w.ws_row/(oldconsolewidth/oldplanewidth));
+      taille(); /*give the height of the console*/
+      planewidth=(w.ws_row/(oldconsolewidth/oldplanewidth)); /*if the height of the console change the plane position change proportionally*/
       planewidth=arrondi(planewidth);
       planelength=(w.ws_col/(oldconsolelength/oldplanelength));
       planelength=arrondi(planelength);
-      oldconsolewidth = w.ws_row;
+      oldconsolewidth = w.ws_row; /*save the old value of the console height*/
       oldconsolelength = w.ws_col;       
-      planedisplay(newhotkey, planewidth, planelength);
+      planedisplay(newhotkey, planewidth, planelength); /*call the fonction to display the plane on the console with the different parameter*/
  
      
-        newhotkey = getchar();
+        newhotkey = getchar(); /*take the user choice*/
         if(newhotkey!=KEYBOARDU && newhotkey!=KEYBOARDD && newhotkey!=KEYBOARDR && newhotkey!=KEYBOARDL && newhotkey!=KEYBOARDEXIT) /*if the character choose by the user is wrong we don't change the position*/
         {
       newhotkey=hotkey;
@@ -134,7 +141,8 @@ int main(void)
         else
         {
       hotkey=newhotkey;
-       switch(hotkey)
+  		}
+       switch(hotkey) /*use a switch to change the plane position according of user choice and the console borders*/
             {
             case KEYBOARDU:
               planewidth=planewidth-1;
@@ -161,7 +169,7 @@ int main(void)
               	planelength = w.ws_col;
               }
             }
-        }
+       
     }
     return 0;
 }
